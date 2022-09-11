36c24caa8374b492111b410f25cc242d11ab09b0
zhousifa <884701384@qq.com>
2022-09-11 10:39:13 +0800

    提交ask.js

---
+++ ask.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: 'the registery is private?'
+  },
+  {
+    type: 'input',
+    name: 'auth',
+    message: 'author?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'description?'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'license?'
+  }
+]
